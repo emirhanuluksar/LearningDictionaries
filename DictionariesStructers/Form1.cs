ee3b6084cbd6584566c5a850103c42b73a3c370a
Emirhan Uluksar <88688871+emirhanuluksar@users.noreply.github.com>
2022-11-02 21:48:20 +0300

    Add project files.

---
+++ DictionariesStructers/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DictionariesStructers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        class Student
+        {
+            public double No { get; set; }
+            public string Name { get; set; }
+            public string Surname { get; set; }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<Student> NewStudents = new List<Student>()
+            {
+                new Student{No=100,Name="John",Surname="Doe"},
+                new Student{No=110,Name="John2",Surname="Doe"},
+                new Student{No=130,Name="John3",Surname="Doe"}
+            };
+
+            Dictionary<Double, Student> List = NewStudents.ToDictionary(N => N.No);
+            foreach (KeyValuePair<Double,Student> student in List)
+            {
+                listBoxControl1.Items.Add(student.Key.ToString() + "-" + student.Value.Name + " " + student.Value.Surname);
+            }
+
+        }
+
+
+
+
+    }
+}
---
+++ DictionariesStructures/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DictionariesStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> NewStudents = new List<Student>()
+            {
+                new Student{No=100,Name="John",Surname="Doe"},
+                new Student{No=110,Name="John2",Surname="Doe"},
+                new Student{No=130,Name="John3",Surname="Doe"}
+            };
+
+            Dictionary<Double, Student> List = NewStudents.ToDictionary(N => N.No);
+            foreach (var student in NewStudents)
+            {
+                Console.WriteLine(student.No.ToString() + "-" + student.Name + " " + student.Surname);
+            }
+        }
+    }
+
+    class Student
+    {
+        public double No { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+    }
+}
